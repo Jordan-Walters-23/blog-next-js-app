6fd485e89da51d715108970a990c919836ecc8f6
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-11-26 14:19:28 +0000

    footer styling social media links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import styles from './footer.module.css'
+import Image from 'next/image'
+import Link from 'next/link'
 
 const Footer = () => {
     return (
-      <div>
+      <div className={styles.container}>
         <div>©2024 Jordan Walters. All rights reserved</div>
-        <div></div>
+        <div className={styles.social}>
+          <Link href={"https://github.com/Jordan-Walters-23"} target="_blank"><Image src="/github-mark-white.png" width={15} height={15} className={styles.icon} alt="github logo"/></Link>
+          <Link href={"https://www.youtube.com"} target="_blank"><Image src="/youtube.png" width={15} height={15} className={styles.icon} alt="Youtube logo"/></Link>
+          <Link href={"https://www.linkedin.com/in/-jordan-walters/"} target="_blank"><Image src="/LinkedIn.png" width={15} height={15} className={styles.icon} alt="LinkedIn logo"/></Link>
+          <Link href={"https://www.instagram.com"} target="_blank"><Image src="/insta.png" width={15} height={15} className={styles.icon} alt="Instagram logo"/></Link>
+        </div>
       </div>
     )
   }
