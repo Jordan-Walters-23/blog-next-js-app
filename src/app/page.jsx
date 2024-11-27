8a7bab3d7be496d0287e8280920374067bb70be3
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-11-27 17:59:29 +0000

    Home structure

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,18 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "../../public/hero.png"
 
 export default function Home() {
-  return <div>Home</div>;
+  return(
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Hello, I'm Jordan. I'm a full stack web developer.</h1>
+        <p className={styles.desc}>Turning ideas into reality</p>
+        <button className={styles.button}>See My Work</button>
+      </div>
+      <div className={styles.item}>
+      <Image src={Hero} alt="Hero" className={styles.img}/>
+      </div>
+    </div>
+  )
 }
