58b12c430923faf89a1dfefdc36b61b6e03a008d
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-11-27 18:31:27 +0000

    Home formatting Social media icons updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
   return(
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Hello, I'm Jordan. I'm a full stack web developer.</h1>
+        <h1 className={styles.title}>Hello, I'm Jordan.<br/>A full stack web developer.</h1>
         <p className={styles.desc}>Turning ideas into reality</p>
         <button className={styles.button}>See My Work</button>
       </div>
