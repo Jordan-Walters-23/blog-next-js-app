9941565d0f5e212b52f55f3844c1b23fdfd58f68
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-11-28 13:54:00 +0000

    about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
+import CoverPhoto from '/public/coverPhoto.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src={CoverPhoto} fill={true} alt='about page cover photo' className={styles.img}/>
+      <div className={styles.imgText}>
+        <h1 className={styles.imgTitle}>creative thinker</h1>
+        <h2 className={styles.imgDesc}>Life long learner</h2>
+      </div>
+      </div>
+      <div className={styles.textContainer}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Who am I?</h1>
+        <p className={styles.desc}>Lorem ipsum odor amet, consectetuer adipiscing elit. Elementum sem etiam senectus id gravida donec ac. Montes diam maecenas neque sem fames lorem nec sit. Dapibus ullamcorper penatibus elementum fermentum bibendum curabitur nunc. Sagittis tortor inceptos mauris suspendisse vitae. Bibendum a venenatis; posuere lectus ipsum diam elementum. Nec rutrum litora dolor sodales maecenas a. Nisi feugiat auctor convallis praesent duis eget elementum urna.</p>
+      </div>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Who am I?</h1>
+      </div>
+      </div>
+
+    </div>
   )
 }
 
