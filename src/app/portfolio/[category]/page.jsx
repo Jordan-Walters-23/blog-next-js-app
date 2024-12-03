a978955843fa43a3cb0a8628a00c7541aca5bf42
Jordan Walters <128825567+Jordan-Walters-23@users.noreply.github.com>
2024-12-03 17:36:26 +0000

    portfolio catagory styling

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import styles from './page.module.css';
+import Button from '../../../components/Button/Button';
+import Image from 'next/image';
 
 
 const Category = ({params}) => {
   console.log(params)
   return (
     <div className={styles.container}>
-      <h1>{params.category}</h1>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}></div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text='See More' url='#'></Button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src='https://images.pexels.com/photos/1181260/pexels-photo-1181260.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt='coders'/>
+        </div>
     </div>
   )
 }
